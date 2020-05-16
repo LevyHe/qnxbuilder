ad784de302abfcbaf9aecb77f59de97c3c638aa8
levy.he@veoneer.com <levy.he@veoneer.com>
2020-05-16 14:11:29 +0800

    reuse the active terminal

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# qnxbuilder README
+# Qnx Builder
 
-qnx build.bat
+A simple tools to set the build environment and call the Makefile under the folder which you right click on.
+
+## Support Cmd
+* QnxBuild - increment build
+* QnxRebuild - rebuild the project
--- build.bat
+++ build.bat
@@ -1,13 +1,15 @@
 @echo off
 SET build_path=%cd%
 set QNX_PATH=C:\Tools\qnx700
-
 call %QNX_PATH%\qnxsdp-env.bat 1>nul
 
 @echo off
 cd %build_path%
-make -j4 clean
-make BUILD_PROFILE=debug all
+if "%1" == "clean" (
+    make clean
+)
+
+make -j8 BUILD_PROFILE=debug all
 
 @echo off
 cd %build_path%
